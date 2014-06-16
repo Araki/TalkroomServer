5c1596e22f5e4322ec07dad1352030fadcde3f51
araki-k <araki-k@PC-1635.local>
2014-06-17 00:36:26 +0900

    no message

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,12 +14,15 @@ Talkroom::Application.configure do
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send
+  require 'tlsmail'
+  Net::SMTP.enable_tls( OpenSSL::SSL::VERIFY_NONE )
+  
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.smtp_settings = {
     :enable_starttls_auto => true,
     :address => 'smtp.gmail.com',
-    :port => '22',
+    :port => '587',
     :domain => 'smtp.gmail.com',
     :authentication => 'plain',
     :user_name => 'admin@talkroom.co',

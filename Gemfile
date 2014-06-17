5eb436dfd466a41a6f77a3dcee1b6f8b9ec02db5
araki-k <araki-k@dhcp-10-11-21-096.22a.klab.org>
2014-06-17 23:18:41 +0900

    tlsmailの設定を挿入

--- Gemfile
+++ Gemfile
@@ -15,6 +15,8 @@ gem 'devise'
 gem 'omniauth'
 gem 'omniauth-facebook'
 
+gem 'tlsmail'
+
 # Gems used only for assets and not required
 # in production environments by default.
 group :assets do
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140615100536) do
+ActiveRecord::Schema.define(:version => 20140617021037) do
 
   create_table "buying_histories", :force => true do |t|
     t.integer  "list_id"
@@ -55,7 +55,7 @@ ActiveRecord::Schema.define(:version => 20140615100536) do
   end
 
   create_table "ios_transactions", :force => true do |t|
-    t.string   "type"
+    t.string   "purchase_type"
     t.string   "product_id"
     t.string   "transaction_id"
     t.datetime "purchase_date"

6609afa2e29f94c010621d07a3363407d544f4d7
araki-k <araki-k@dhcp-10-11-21-096.22a.klab.org>
2014-06-17 21:05:39 +0900

    MySQLエラー「type」が予約語だったので「purchase_type」にカラム名を変更

---
+++ db/migrate/20140617021037_rename_column_ios_transactions.rb
@@ -0,0 +1,8 @@
+class RenameColumnIosTransactions < ActiveRecord::Migration
+  def up
+    rename_column :ios_transactions, :type, :purchase_type
+  end
+  def down
+    rename_column :ios_transactions, :purchase_type, :type
+  end
+end

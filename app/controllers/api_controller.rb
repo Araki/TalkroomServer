61a005b83b88a57ecb0b916af00319140e29a61c
araki-k <araki-k@dhcp-10-11-21-096.22a.klab.org>
2014-07-10 23:08:56 +0900

    Facebookプロフィール画像をS3にアップロードする改修７

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -948,8 +948,7 @@ class ApiController < ApplicationController
     logger.info("original_filename:#{file.original_filename}")
     object = bucket.objects[file_full_path] #objectというオブジェクトの作成
     
-    url = "https://fbcdn-profile-a.akamaihd.net/hprofile-ak-xpa1/t1.0-1/s200x200/225678_10150172927029204_5629112_n.jpg
-"
+    url = "http://ww1.sinaimg.cn/large/9a95b0aagw1e6d5mhmmcjj20uc1a1wp8.jpg"
     open(url) do |data|
       logger.info("IMG:#{data}")
     end

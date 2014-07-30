307ef41e68c7bbf823f3be0209d15c9ac75f9442
araki-k <araki-k@dhcp-10-11-21-096.22a.klab.org>
2014-07-30 21:01:48 +0900

    リワード開発１６

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1362,8 +1362,9 @@ class ApiController < ApplicationController
       @ca_reward.pid = params[:pid]
       
       duplication_flag = CaReward.
-                         where('cid = ? AND list_id = ? AND action_date = ? AND pid = ?', 
+                         #where('cid = ? AND list_id = ? AND action_date = ? AND pid = ?', 
                          #params[:cid].to_i, params[:uid].to_i, params[:action_date], params[:pid].to_i).
+                         where('cid = ? AND list_id = ? AND pid = ?',
                          params[:cid].to_i, params[:uid].to_i, params[:pid].to_i).
                          exists?
       
